625774cf5c7177eca09cbbd2310818d8822af525
sarkisovTu <sarkisov1337@gmail.com>
2022-12-27 15:18:41 +0300

    feat: loadEnvVariables function

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import renderer from 'vite-plugin-electron-renderer';
 import { fileURLToPath, URL } from 'url';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+
+const loadEnvVariables = (mode: string): void => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '',
-  mode: 'production',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL("./src", import.meta.url)),
-    }
-  },
-  plugins: [
-    vue(),
-    renderer({
-      nodeIntegration: true
-    })
-  ]
+export default defineConfig((init) => {
+  loadEnvVariables(init.mode);
+  return {
+    base: '',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL("./src", import.meta.url)),
+      }
+    },
+    plugins: [
+      vue(),
+      renderer({
+        nodeIntegration: true
+      })
+    ]
+  }
 })

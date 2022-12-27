496292bd4d71b9453a30095f9d2b2e90a7e618f6
sarkisovTu <sarkisov1337@gmail.com>
2022-12-27 19:20:38 +0300

    refactor: Electron process setup

---
+++ electron/main/index.ts
@@ -0,0 +1,57 @@
+import { app, BrowserWindow } from 'electron';
+
+import { release } from 'node:os';
+import { join } from 'node:path';
+
+process.env.DIST_ELECTRON = join(__dirname, '..');
+process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
+process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
+  ? join(process.env.DIST_ELECTRON, '../public')
+  : process.env.DIST;
+
+if (release().startsWith('6.1')) app.disableHardwareAcceleration();
+
+if (process.platform === 'win32') app.setAppUserModelId(app.getName());
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+
+let win: BrowserWindow | null = null;
+
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, 'index.html');
+
+async function createWindow() {
+  win = new BrowserWindow({
+    icon: join(process.env.PUBLIC, 'favicon.ico'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(url);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  win = null;
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});

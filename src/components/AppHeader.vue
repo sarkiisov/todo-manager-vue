9310230c90bea1805d6c2ca2207543eb491d1357
sarkisovTu <sarkisov1337@gmail.com>
2023-01-01 19:04:17 +0300

    feat: AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h2 class="header__text">
+      <slot />
+    </h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.header {
+  height: 100px;
+  display: flex;
+  align-items: center;
+  user-select: none;
+}
+.header__text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: 500;
+  color: var(--c-text);
+}
+</style>

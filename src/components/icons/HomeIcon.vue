31b8a6a54688eec3a0e98c61aa938f82c9623554
sarkisovTu <sarkisov1337@gmail.com>
2023-01-01 13:41:10 +0300

    feat: add new svg icons

---
+++ src/components/icons/HomeIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppSvgBase>
+    <path xmlns="http://www.w3.org/2000/svg" d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+    <polyline xmlns="http://www.w3.org/2000/svg" points="9 22 9 12 15 12 15 22"/>
+  </AppSvgBase>
+</template>
+
+<script setup lang="ts">
+import AppSvgBase from './AppSvgBase.vue';
+</script>
---
+++ src/components/icons/StarIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <AppSvgBase>
+    <polygon xmlns="http://www.w3.org/2000/svg" points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+  </AppSvgBase>
+</template>
+
+<script setup lang="ts">
+import AppSvgBase from './AppSvgBase.vue';
+</script>

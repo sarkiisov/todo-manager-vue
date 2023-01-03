415405cadb40eaaf4c6641649d1005234bf4e714
sarkisovTu <sarkisov1337@gmail.com>
2023-01-03 19:24:10 +0300

    feat: router setup

---
+++ src/router/index.ts
@@ -0,0 +1,56 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { useCollectionsStore } from '@/store/collections';
+import AppHeader from '@/components/AppHeader.vue';
+import CollectionHeader from '@/components/CollectionHeader.vue';
+import Overview from '@/views/Overview.vue';
+import Important from '@/views/Important.vue';
+import Collection from '@/views/Collection.vue';
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/collection',
+    },
+    {
+      path: '/collection',
+      children: [
+        {
+          path: '',
+          name: 'overview',
+          components: {
+            header: AppHeader,
+            content: Overview,
+          },
+        },
+        {
+          path: 'important',
+          name: 'important',
+          components: {
+            header: AppHeader,
+            content: Important
+          }
+        },
+        {
+          path: ':id',
+          name: 'collection',
+          components: {
+            header: CollectionHeader,
+            content: Collection
+          }
+        }
+      ]
+    }
+  ]
+});
+
+router.beforeEach((to) => {
+  if (to.name === 'collection') {
+    const collections = useCollectionsStore();
+    collections.setCurrentCollectionId(to.params.id as string);
+  }
+  return true;
+});
+
+export default router;

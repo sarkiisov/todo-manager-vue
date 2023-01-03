fd205be797dce763d36b3e60b2eb177033a92543
sarkisovTu <sarkisov1337@gmail.com>
2023-01-03 19:22:35 +0300

    feat: app use router & pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
-import './style.css';
+import './assets/css/theme.css';
+import './assets/css/fonts.css';
+import './assets/css/reset.css';
+import { createPinia } from 'pinia';
 import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .mount('#app');

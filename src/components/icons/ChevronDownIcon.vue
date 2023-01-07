7b3de67bf5a75a76da476fd089d43e869893b7c2
sarkisovTu <sarkisov1337@gmail.com>
2023-01-07 20:20:43 +0300

    feat: add new svg icons

---
+++ src/components/icons/ChevronDownIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <AppSvgBase>
+    <polyline xmlns="http://www.w3.org/2000/svg" points="6 9 12 15 18 9"/>
+  </AppSvgBase>
+</template>
+
+<script setup lang="ts">
+import AppSvgBase from './AppSvgBase.vue';
+</script>

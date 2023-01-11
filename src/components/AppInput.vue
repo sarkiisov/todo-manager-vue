2f31ef2af96d7f5116b8a7693c365bb228c9d8c7
sarkisovTu <sarkisov1337@gmail.com>
2023-01-11 19:25:24 +0300

    feat: Input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    class="input"
+    spellcheck="false"
+    :placeholder="props.placeholder"
+    :value="props.modelValue"
+    @input="updateModelValue"
+  >
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  modelValue: { type: String },
+  placeholder: { type: String }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const updateModelValue = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  emit('update:modelValue', target.value);
+};
+
+</script>
+
+<style scoped>
+.input {
+  height: 40px;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  background: var(--c-bg);
+  color: var(--c-text);
+  border-bottom: 1px solid var(--c-border);
+}
+.input::placeholder {
+  color: var(--c-border);
+}
+.input::selection {
+  background: var(--c-bg-dark);
+}
+</style>

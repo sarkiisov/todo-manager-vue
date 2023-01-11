05c940074919dcc2bbf30d185dc8fdb863f67488
sarkisovTu <sarkisov1337@gmail.com>
2023-01-11 19:17:33 +0300

    feat: Button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.button {
+  min-width: 150px;
+  height: 40px;
+  padding: 0px 12px;
+  border-radius: 4px;
+  font-size: 16px;
+  background: var(--c-bg);
+  border: 1px solid var(--c-border);
+  color: var(--c-text);
+}
+.button:hover {
+  cursor: pointer;
+  background: var(--c-bg-mute);
+}
+</style>

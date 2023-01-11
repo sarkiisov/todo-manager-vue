68876000b76e753568dc9dc7233f194e09af0763
sarkisovTu <sarkisov1337@gmail.com>
2023-01-11 19:24:18 +0300

    feat: SectionHeader component

---
+++ src/components/AppSectionHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="section-header">
+    <h3 class="section-header__body">
+      <slot />
+    </h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.section-header {
+  padding-bottom: 24px;
+}
+.section-header ~
+.section-header {
+  padding-top: 20px;
+}
+.section-header__body {
+  user-select: none;
+  font-size: 16px;
+  font-weight: 500;
+  color: var(--c-text);
+}
+</style>

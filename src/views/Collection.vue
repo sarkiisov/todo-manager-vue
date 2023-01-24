3ad6e7ace6dd5d8fb01aaa053ef45a44425643e3
sarkisovTu <sarkisov1337@gmail.com>
2023-01-24 18:00:14 +0300

    feat: add CollectionStatistic to Collection view

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,9 +1,11 @@
 <template>
+  <CollectionStatistic />
   <TodoForm :collection-id="collectionsStore.currentCollectionId"/>
   <TodoList :todos="todoStore.currentCollectionTodos"/>
 </template>
 
 <script setup lang="ts">
+import CollectionStatistic from '@/components/CollectionStatistic.vue';
 import TodoForm from '@/components/TodoForm.vue';
 import TodoList from '@/components/TodoList.vue';
 import { useCollectionsStore } from '@/store/collections';
@@ -12,6 +14,3 @@ import { useTodosStore } from '@/store/todos';
 const todoStore = useTodosStore();
 const collectionsStore = useCollectionsStore();
 </script>
-
-<style>
-</style>

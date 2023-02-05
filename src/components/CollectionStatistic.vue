1e8fe0ca32bb30762a90c5bdd7b60d2631e985f2
sarkisovTu <sarkisov1337@gmail.com>
2023-02-05 16:53:39 +0300

    fix: handle zero division in todos completition

--- src/components/CollectionStatistic.vue
+++ src/components/CollectionStatistic.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collection-statistic">
-    <ProgressPie :percentage="completitionPercent"/>
+    <CollectionProgressPie :percentage="completitionPercent"/>
     <span class="collection-statistic__body">{{ `${completedTodosCount} / ${allTodosCount}` }}</span>
   </div>
 </template>
@@ -8,13 +8,13 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useTodosStore } from '@/store/todos';
-import ProgressPie from './CollectionProgressPie.vue';
+import CollectionProgressPie from '@/components/CollectionProgressPie.vue';
 
 const todoStore = useTodosStore();
 
 const allTodosCount = computed(() => todoStore.currentCollectionTodos.length);
 const completedTodosCount = computed(() => todoStore.currentCollectionTodos.filter((object) => object.isCompleted).length);
-const completitionPercent = computed(() => Math.round(100 * (completedTodosCount.value / allTodosCount.value)));
+const completitionPercent = computed(() => Math.round(100 * (allTodosCount.value !== 0 ? (completedTodosCount.value / allTodosCount.value) : 0)));
 </script>
 
 <style scoped>
